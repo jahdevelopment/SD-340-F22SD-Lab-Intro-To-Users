fa41c8aa54aabeacbd1e6ad5240961bc8605fe99
juanhernandezdeveloper <jualherac@gmail.com>
2023-04-05 14:24:11 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD_340_F22SD_Lab_Intro_To_Users.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
