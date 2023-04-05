313527e8c70b05f72896b38a85e411ca8c36de88
juanhernandezdeveloper <jualherac@gmail.com>
2023-04-05 14:38:04 -0500

    Creation of context and user classes

---
+++ Areas/Identity/Data/BankingAppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SD_340_F22SD_Lab_Intro_To_Users.Areas.Identity.Data;
+
+namespace SD_340_F22SD_Lab_Intro_To_Users.Data;
+
+public class BankingAppContext : IdentityDbContext<BankingAppUser>
+{
+    public BankingAppContext(DbContextOptions<BankingAppContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SD_340_F22SD_Lab_Intro_To_Users.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BankingAppContextConnection") ?? throw new InvalidOperationException("Connection string 'BankingAppContextConnection' not found.");
+
+builder.Services.AddDbContext<BankingAppContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<BankingAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<BankingAppContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
